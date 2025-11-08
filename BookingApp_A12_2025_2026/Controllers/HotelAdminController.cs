542d8bbe23ec3bb0ad96362801ef4a54c6da5c71
shadi adnan <shadiadnan@hotmail.com>
2025-11-08 17:08:39 +0200

    لوحة تحكم مدير الفندق

--- BookingApp_A12_2025_2026/Controllers/HotelAdminController.cs
+++ BookingApp_A12_2025_2026/Controllers/HotelAdminController.cs
@@ -13,7 +13,7 @@ namespace BookingApp_A12_2025_2026.Controllers
             int Hotel_Id = int.Parse(User.Identity.Name);
             Hotel h1=Hotel.GetHotelById(Hotel_Id);
             ViewBag.h1 = h1;
-            return View();
+            return View("HotelCP");
         }
 
         public async Task<IActionResult> LogoutView()
