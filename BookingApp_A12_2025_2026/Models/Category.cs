f9b1a64826b9a0265425891eed1a5c95d4236c93
Shadi <Shadi@T12>
2025-11-18 09:59:27 +0200

    grtg

---
+++ BookingApp_A12_2025_2026/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace BookingApp_A12_2025_2026.Models
+{
+    public class Category
+    {
+        public int Category_Id {  get; set; }
+        public string Category_Name { get; set; }
+        public string Category_Photo { get; set; }
+
+        public string Category_Description { get; set; }
+
+        //get all product from db
+
+
+    }
+}
